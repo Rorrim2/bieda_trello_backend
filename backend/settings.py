c1720206dc02fae460647055ab04e96125a1979f
Hergoln <juliusz.malka@o2.pl>
2020-10-31 21:37:31 +0100

    patched settings

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'skeleton',
+    'skeleton.apps.SkeletonConfig',
     "django_filters",
     'corsheaders'
 ]
