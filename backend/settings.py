6a7a6fe13592731b9868a8b742f0c53badc28a44
Hergoln <juliusz.malka@o2.pl>
2020-10-31 21:21:42 +0100

    removed duplicate skeleton from installed apps

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'skeleton.apps.SkeletonConfig',
     'skeleton',
     "django_filters",
     'corsheaders'
