9ae5260b1c29f99fc7b434ceed385bcbafe99af9
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-11-06 02:36:23 +0100

    Prepared sample mutation for registration/login purpose, updated UserModel to be more compatible with Django mechanisms, added django-graphql-jwt for authentication purpose. Struggling with sqlite3 and setting email as username for usermodel

--- skeleton/users/model.py
+++ skeleton/users/model.py
@@ -1,8 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from ..utils import crypto
 
-class UserModel(models.Model):
-    name = models.CharField(max_length=100)
+class UserModel(AbstractUser):
+    name = models.CharField(max_length=100) 
     last_name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
-    salt = models.CharField(max_length=64, blank=True)
-    hashed_pwd = models.TextField(blank=True)
+    email = models.EmailField(max_length=254, unique=True)
+    salt = models.CharField(max_length=64)
+    hashed_pwd = models.TextField()
+    is_superuser = models.BooleanField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'last_name']
+
+    def set_salt(self):
+        self.salt = crypto.generate_salt()
+
+    def set_password(self, password: str):
+        if self.salt in [None, '']:
+            self.set_salt()
+        self.hashed_pwd = crypto.hash_passwd(self.salt, password)
+
+    def check_password(self, raw_password: str):
+        return crypto.validate_passwd(self.salt, raw_password, self.hashed_pwd)
--- skeleton/users/schema.py
+++ skeleton/users/schema.py
@@ -1,7 +1,8 @@
 from graphene_django import DjangoObjectType
 import graphene
 from graphql.execution.base import ResolveInfo
-
+from graphql_jwt import shortcuts
+from ..utils import crypto
 from .model import UserModel
 
 # this file is something like top-level urls.py
@@ -15,7 +16,56 @@ class UserType(DjangoObjectType):
         
 class Query(graphene.ObjectType):
     users = graphene.List(UserType)
+    user = graphene.relay.Node.Field(UserType)
 
     def resolve_users(self, info: ResolveInfo, **kwargs):
         print(info.path)
         return UserModel.objects.all()
+
+class LoginUser(graphene.Mutation):
+    user = graphene.relay.Node.Field(UserType)
+    success = graphene.Boolean()
+    token = graphene.String()
+
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+
+    def mutate(self, info, email: str, password: str):
+        user = None
+        success = False
+        token=''
+        if UserModel.objects.filter(email=email).exists():
+            user = UserModel.objects.get(email=email)
+            if crypto.validate_passwd(user.salt, password, user.hashed_pwd):
+                token = shortcuts.get_token(user)
+                success = True
+            else:
+                user = None
+
+        return LoginUser(user=user, success=success, token=token)
+
+class RegisterUser(graphene.Mutation):
+    user = graphene.relay.Node.Field(UserType)
+    success = graphene.Boolean()
+    token = graphene.String()
+
+    class Arguments:
+        email = graphene.String(required=True)
+        password = graphene.String(required=True)
+        name = graphene.String(required=True)
+        last_name = graphene.String(required=True)
+
+    def mutate(self, info, email: str, password: str, name: str, last_name: str):
+        user = None
+        success = False
+        token=''
+        if not UserModel.objects.filter(email=email).exists():
+            user = UserModel(name=name, last_name=last_name, email=email)
+            user.set_salt()
+            user.set_password(password)
+            user.save()
+            
+            token = shortcuts.get_token(user)
+
+        return RegisterUser(user=user, success=success, token=token)
