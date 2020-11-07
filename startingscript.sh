d408d894b80e6ee8a7fc9309e544bb4f6551411e
Hergoln <juliusz.malka@o2.pl>
2020-11-07 14:25:08 +0100

    updated exemplaryData files and revealed users in startingscript file

--- startingscript.sh
+++ startingscript.sh
@@ -1,4 +1,8 @@
 python manage.py migrate
 python manage.py migrate --run-syncdb
-python manage.py loaddata .\exemplaryData\users.json
-python manage.py loaddata .\exemplaryData\boards.json
+# email : password
+# Johny.cache@gmail.com : sussPass
+# Spoopy.skeleton@o2.pl : scaryPass
+# Gary.theGrail@.com : holyPass
+python manage.py loaddata exemplaryData/users.json
+python manage.py loaddata exemplaryData/boards.json
