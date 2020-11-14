c3891c7697e1e11a85561aaba8849c0a4650f58a
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-11-14 15:46:51 +0100

    Fixed issues with UserModel, moved contents of exemplaryUserData.json to users.json, simplified script job.

--- skeleton/users/model.py
+++ skeleton/users/model.py
@@ -1,7 +1,9 @@
-from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.base_user import BaseUserManager
 from django.db import models
 from ..utils import crypto
 from django.contrib.auth.models import PermissionsMixin
+import unicodedata
+from django.utils.translation import gettext_lazy as _
 
 class UserManager(BaseUserManager):
     use_in_migrations = True
@@ -32,7 +34,9 @@ class UserManager(BaseUserManager):
     def get_by_natural_key(self, username):
         return self.get(email=username)
     
-class UserModel(AbstractBaseUser):
+class UserModel(models.Model):
+
+    last_login = models.DateTimeField(_('last login'), blank=True, null=True)
     name = models.CharField(max_length=100) 
     last_name = models.CharField(max_length=100)
     email = models.EmailField(max_length=254, unique=True)
@@ -40,9 +44,51 @@ class UserModel(AbstractBaseUser):
     hashed_pwd = models.TextField()
     is_superuser = models.BooleanField(blank=True, default=False)
     is_staff = models.BooleanField(default=False)
+    is_active = True
     objects = UserManager()
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name', 'last_name']
+
+    def __str__(self):
+        return self.get_username()
+
+    def get_username(self):
+        return getattr(self, self.USERNAME_FIELD)
+
+    def clean(self):
+        setattr(self, self.USERNAME_FIELD, self.normalize_username(self.get_username()))
+
+    def natural_key(self):
+        return (self.get_username(),)
+
+    @property
+    def is_anonymous(self):
+        """
+        Always return False. This is a way of comparing User objects to
+        anonymous users.
+        """
+        return False
+
+    @property
+    def is_authenticated(self):
+        """
+        Always return True. This is a way to tell if the user has been
+        authenticated in templates.
+        """
+        return True
+
+    @classmethod
+    def get_email_field_name(cls):
+        try:
+            return cls.EMAIL_FIELD
+        except AttributeError:
+            return 'email'
+
+    @classmethod
+    def normalize_username(cls, username):
+        return unicodedata.normalize('NFKC', username) if isinstance(username, str) else username
+
 
     def get_all_permissions(self, obj=None):
         return PermissionsMixin.get_all_permissions() if self.is_superuser else set()
