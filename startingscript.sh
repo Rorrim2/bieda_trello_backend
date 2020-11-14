e29d8276e2c21281e6a90830417445f8def679e1
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-11-14 23:25:39 +0100

    Added verbose output for migrate commands

--- startingscript.sh
+++ startingscript.sh
@@ -1,16 +1,16 @@
 ls
 rm dec0324fegi0e9
-python manage.py flush --no-input
+python manage.py flush --no-input -v 3
 python manage.py makemigrations
-python manage.py migrate
-python manage.py migrate --run-syncdb
+python manage.py migrate -v 3
+python manage.py migrate --run-syncdb -v 3
 # email : password
 # Johny.cache@gmail.com : sussPass
 # Spoopy.skeleton@o2.pl : scaryPass
 # Gary.theGrail@.com : holyPass
 # elmohellmo@sesame-street.com : password1234
 # john_cena@berkeley.edu.com : 123456789
-python manage.py loaddata exemplaryData/users.json
+python manage.py loaddata exemplaryData/users.json -v 3
 python manage.py loaddata exemplaryData/boards.json
 python manage.py loaddata exemplaryData/lists.json
 python manage.py loaddata exemplaryData/cards.json
