0369e4131d490448038d5a1a033c11ff0930ae4b
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-11-19 20:49:09 +0100

    implemented set of jwt_utils to handle logout

--- skeleton/utils/jwt_utils.py
+++ skeleton/utils/jwt_utils.py
@@ -1,5 +1,43 @@
+from skeleton.users.model import UserModel
 from .crypto import generate_salt, generate_random_str
+from ..models import UserModel
+from graphql_jwt.utils import get_user_by_payload ,jwt_payload, jwt_encode, jwt_decode, get_payload
+import graphql_jwt.exceptions as exceptions
+import jwt
+from typing import Dict
 
 def create_jwt_id():
     return f"_{generate_salt(15)}__{generate_random_str(30)}_"
+
+def get_payload(user: UserModel, context=None):
+    payload = jwt_payload(user, context)
+    payload["jti"] = user.jwt_salt
+    return payload
+
+def get_token(user: UserModel, context=None):
+    payload = get_payload(user, context)
+    return jwt_encode(payload, context)
+
+def decode_token(token: str, context=None):
+    payload = {}
+    def __verify_payload(payload: Dict):
+        user = get_user_by_payload(payload)
+        jti = ""
+        try:
+            jti = payload["jti"]
+        except:
+            raise jwt.MissingRequiredClaimError("jti")
+        if(user.jwt_salt != jti):
+            raise jwt.InvalidTokenError("Token expired by user-logout request")
+
+    try:
+        payload = jwt_decode(token, context)
+        __verify_payload(payload)        
+    except jwt.ExpiredSignature:
+        raise exceptions.JSONWebTokenExpired()
+    except jwt.DecodeError:
+        raise exceptions.JSONWebTokenError('Error decoding signature')
+    except jwt.InvalidTokenError as j:
+        raise exceptions.JSONWebTokenError('Invalid token\n' + j.message)
+    return payload
 
