c951a5cdc81b17f4fb19724252567679a8121213
Hergoln <juliusz.malka@o2.pl>
2020-11-23 17:08:14 +0100

    added Boards, Lists and Cards mutations

--- skeleton/schema.py
+++ skeleton/schema.py
@@ -17,9 +17,10 @@ class Query(UserQuery, BoardQuery, ListQuery, CardQuery, graphene.ObjectType):
     pass
 
 
-class Mutation(UserMutation, graphene.ObjectType):
+class Mutation(UserMutation, BoardMutation, ListMutation, CardMutation, graphene.ObjectType):
     verify_token = graphql_jwt.Verify.Field()
     refresh_token = graphql_jwt.Refresh.Field()
     revoke_token = graphql_jwt.Revoke.Field()
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
