72c874203bbc618a3e66057a3d6f1a0c85c27b5e
Kamil Chrustowski <chrustek123456789@gmail.com>
2020-11-26 01:05:08 +0100

    implemented JWTAuthenticationMiddleware

--- skeleton/utils/middleware.py
+++ skeleton/utils/middleware.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import AnonymousUser
+from django.http.request import HttpRequest
 from django.utils import timezone
+from django.utils.functional import SimpleLazyObject
 from ..models import UserModel
+from graphql_jwt import shortcuts
 
 class UpdateLastActivityMiddleware(object):
     
@@ -10,5 +14,22 @@ class UpdateLastActivityMiddleware(object):
         if request.user.is_authenticated:
             UserModel.objects.filter(pk=request.user.id).update(last_login=timezone.now())
         response = self.get_response(request)
-
+        # if request.user.is_authenticated:
+        #     response['Set-Cookie'] = (f'token={shortcuts.get_token(UserModel.objects.filter(pk=request.user.id))}')
         return response
+
+class JWTAuthenticationMiddleware(object):
+    
+    def __init__(self, get_response):
+        self.get_response = get_response
+    def __call__(self, request):
+        request.user = SimpleLazyObject(lambda: get_user(request))
+        return self.get_response(request)
+
+def get_user(request: HttpRequest):
+    try:
+        return shortcuts.get_user_by_token(request.headers["Authorization"].replace("Bearer ", ""))
+    except Exception as e:
+        print(e)
+        return AnonymousUser()
+
